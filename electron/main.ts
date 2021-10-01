8550b4b6b9c5b32a492c504d7a3c2385af7d9f43
Matthew Murphy <matt@mattmurphy.ca>
2021-09-30 20:33:26 -0600

    init

--- electron/main.ts
+++ electron/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, ipcMain } from 'electron'
+import { BrowserWindow, app, ipcMain } from 'electron'
 
 let mainWindow: BrowserWindow | null
 
@@ -10,48 +10,54 @@ declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string
 //     ? process.resourcesPath
 //     : app.getAppPath()
 
-function createWindow () {
-  mainWindow = new BrowserWindow({
-    // icon: path.join(assetsPath, 'assets', 'icon.png'),
-    width: 1100,
-    height: 700,
-    backgroundColor: '#191622',
-    webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true,
-      preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY
-    }
-  })
+function createWindow() {
+	mainWindow = new BrowserWindow({
+		// icon: path.join(assetsPath, 'assets', 'icon.png'),
+		width: 500,
+		height: 500,
+		transparent: true,
+		frame: false,
+		backgroundColor: '#00FFFFFF',
+		hasShadow: false,
+		resizable: true,
+		alwaysOnTop: true,
+		webPreferences: {
+			nodeIntegration: false,
+			contextIsolation: true,
+			preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
+		},
+	})
 
-  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY)
+	mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY)
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+	mainWindow.on('closed', () => {
+		mainWindow = null
+	})
 }
 
-async function registerListeners () {
-  /**
-   * This comes from bridge integration, check bridge.ts
-   */
-  ipcMain.on('message', (_, message) => {
-    console.log(message)
-  })
+async function registerListeners() {
+	/**
+	 * This comes from bridge integration, check bridge.ts
+	 */
+	ipcMain.on('message', (_, message) => {
+		console.log(message)
+	})
 }
 
-app.on('ready', createWindow)
-  .whenReady()
-  .then(registerListeners)
-  .catch(e => console.error(e))
+app
+	.on('ready', createWindow)
+	.whenReady()
+	.then(registerListeners)
+	.catch((e) => console.error(e))
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
 })
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow()
+	}
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,125 @@
-import { GlobalStyle } from './styles/GlobalStyle'
+import { MouseEvent, useEffect, useState } from 'react'
 
-import { Greetings } from './components/Greetings'
+import { GlobalStyle } from './styles/GlobalStyle'
+import styled from 'styled-components'
+
+const PseudoWindow = styled.div`
+	position: absolute;
+	top: 40px;
+	bottom: 40px;
+	width: 100%;
+	background-color: rgba(255, 255, 255, 0.1);
+	border-top: 1px solid white;
+	border-bottom: 1px solid white;
+	-webkit-app-region: drag;
+	cursor: move;
+`
+
+const MiddleLine = styled.div`
+	position: absolute;
+	top: 50%;
+	width: 100%;
+	border-top: 1px solid white;
+`
+
+const BottomResize = styled.div`
+	background-color: rgba(255, 255, 255, 0.5);
+	position: absolute;
+	bottom: 0;
+	height: 10px;
+	width: 100%;
+	color: black;
+	-webkit-app-region: no-drag;
+	&:hover {
+		cursor: ns-resize;
+	}
+`
+
+const Container = styled.div`
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	width: 100%;
+	border: 1px solid white;
+`
 
 export function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <Greetings />
-    </>
-  )
+	const [mouseDown, setMouseDown] = useState(false)
+	const [prevY, setPrevY] = useState<number | undefined>()
+	const [cursorPresent, setCursorPresent] = useState<boolean | undefined>()
+	const [cursorDraggedOffWindow, setCursorDraggedOffWindow] = useState(false)
+	const [moveInterval, setMoveInterval] = useState<undefined | number>()
+
+	useEffect(() => {
+		// keep moving window down until cursor is back
+		if (cursorDraggedOffWindow) {
+			if (cursorPresent !== undefined) {
+				console.log('cursor dragged off', cursorPresent)
+				const interval = window.setInterval(() => {
+					window.resizeBy(0, 5)
+				}, 0)
+				setMoveInterval(interval)
+			}
+		}
+	}, [cursorDraggedOffWindow])
+
+	useEffect(() => {
+		if (cursorPresent) setCursorDraggedOffWindow(false)
+	}, [cursorPresent])
+
+	function handleMouseMove(e: MouseEvent) {
+		// const offset = window.innerHeight - e.pageY
+
+		// if (!mouseDown) {
+		// 	setPrevY(undefined)
+		// }
+		if (mouseDown && prevY) {
+			// const differenceY = e.screenY - prevY
+			window.resizeTo(400, e.pageY + 50)
+			setPrevY(e.screenY)
+		}
+	}
+	function handleMouseDown(e: MouseEvent) {
+		if (e.button === 2) {
+			// left click
+			resizeTo(400, 400)
+		}
+		setMouseDown(true)
+		setPrevY(e.screenY)
+		// setInitialY(e.screenY)
+	}
+	function handleMouseUp(e: MouseEvent) {
+		setMouseDown(false)
+	}
+	function handleMouseEnter(e: MouseEvent) {
+		console.log('mouse enter')
+		setCursorPresent(true)
+		clearInterval(moveInterval)
+		if (mouseDown) {
+			// cursor was dragged off-screen
+			console.log('mousedown re-enter')
+			window.resizeTo(400, e.pageY + 50)
+		}
+	}
+	function handleMouseLeave(e: MouseEvent) {
+		setCursorPresent(false)
+		if (mouseDown) {
+			setCursorDraggedOffWindow(true)
+			// setMouseDown(false)
+		}
+	}
+	return (
+		<Container
+			onMouseMove={handleMouseMove}
+			onMouseUp={handleMouseUp}
+			onMouseLeave={handleMouseLeave}
+			onMouseEnter={handleMouseEnter}
+		>
+			<GlobalStyle />
+			<PseudoWindow>
+				<MiddleLine />
+				<BottomResize onMouseDown={handleMouseDown} />
+			</PseudoWindow>
+		</Container>
+	)
 }
